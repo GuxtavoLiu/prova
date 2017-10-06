51360b806e44d464f2a103afb2fd7229ee2ab9e4
Aluno <Aluno@172.32.1.18>
2017-10-05 21:06:08 -0300

    Criando funções

--- src/algoritmos/Algoritmos.java
+++ src/algoritmos/Algoritmos.java
@@ -1,11 +1,21 @@
-
 package algoritmos;
 
 public class Algoritmos {
 
-   
-    public static void main(String[] args) {
-  
+    public static void quadrado(double n) {
+        double r = n * n;
     }
-    
+
+    public static double metade(double v) {
+        double r = v / 2;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        double raiz;
+        raiz = Math.sqrt(15);
+        System.out.print("A raiz é " + raiz);
+
+    }
+
 }
