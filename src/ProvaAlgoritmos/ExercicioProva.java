2a62cc6647063f78b58e2f2260a06fafc7ca1797
Gustavo Liu <gustavo_santostr@hotmail.com>
2017-11-24 00:04:35 -0200

    Prova Algoritmos

---
+++ src/ProvaAlgoritmos/ExercicioProva.java
@@ -0,0 +1,60 @@
+package ProvaAlgoritmos;
+
+public class ExercicioProva{
+
+    /*Questão 2
+    public static String solicitarRazaoSocial(String rs) {
+        Scanner e = new Scanner(System.in);
+        String y = e.nextLine();
+        System.out.println("Insira o nome da Razão Social: ");
+        y = rs;
+        return rs;
+    }
+     */
+ /*Questão 3
+    public static int encontrarPosicao(int[] x) {
+        int maior = 0;
+
+        for (int y = 0; y > x.length; y++) {
+            if (x[y] > maior) {
+                maior = x[y];
+            }
+        }
+        int menor = maior;
+        for (int y = 0; y > x.length; y++) {
+
+            if (x[y] < menor) {
+                menor = x[y];
+            }
+        }
+
+        return menor;
+    }
+     */
+ /*Questão 4
+    public static int calcularSoma(int[] x) {
+        int soma = 0;
+
+        for (int y = 0; y > x.length; y++) {
+            soma = soma + x[y];
+        }
+        return soma;
+    }
+     */
+ /*Questão 5 
+    public static int multiplicarVetor(int[] x) {
+        int t;
+        int[] mult = new int[t];
+        for (int y = 0; y > x.length; y++) {
+            mult[t] = x[y] * y;
+        }
+
+        return mult[t];
+    }
+     */
+    public static void main(String[] args) {
+       
+
+    }
+
+}
